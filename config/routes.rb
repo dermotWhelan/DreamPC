42174487db7371ceb72c25e91929ee856fc4a0f8
Patrick W <x16119266@student.ncirl.ie>
2017-12-11 21:11:13 +0000

    Seventh Commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get '/members', to:'static_pages#members'
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
+  
+  get '/admin', to: 'user#admin_login'
+  get '/logout', to: 'user#logout'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
